efdd4c65364897471a6d70f38b62913f58b901dc
Gianbattista Vivolo <gianbattista.vivolo@gmail.com>
2025-04-26 18:12:26 +0200

    Implement Garbage base class with core attributes and methods

--- src/lib/Garbage.cpp
+++ src/lib/Garbage.cpp
@@ -1,2 +1,18 @@
 #include "Garbage.hpp"
 
+Garbage::Garbage(const std::string& name, double weight,bool isRecyclable)
+    : m_name(name), m_weight(weight), m_isRecyclable(isRecyclable)
+{
+}
+
+std::string Garbage::getName() const {
+    return m_name;
+}
+
+double Garbage::getWeight() const {
+    return m_weight;
+}
+
+bool Garbage::isRecyclable() const {
+    return m_isRecyclable;
+}
